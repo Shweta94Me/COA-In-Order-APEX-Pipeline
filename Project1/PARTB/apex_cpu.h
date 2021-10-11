fb6c3751fbb6d49ee0f0d9540aa00abb20f0a84c
Shweta94Me <mestryshweta30@gmail.com>
2021-10-10 20:14:53 -0400

    Project added

---
+++ Project1/PARTB/apex_cpu.h
@@ -0,0 +1,79 @@
+/*
+ * apex_cpu.h
+ * Contains APEX cpu pipeline declarations
+ *
+ * Author:
+ * Copyright (c) 2020, Gaurav Kothari (gkothar1@binghamton.edu)
+ * State University of New York at Binghamton
+ */
+#ifndef _APEX_CPU_H_
+#define _APEX_CPU_H_
+
+#include "apex_macros.h"
+
+/* Format of an APEX instruction  */
+typedef struct APEX_Instruction
+{
+    char opcode_str[128];
+    int opcode;
+    int rd;
+    int rs1;
+    int rs2;
+    int rs3;  /*source register added for STR instruction */
+    int imm;
+} APEX_Instruction;
+
+/* Model of CPU stage latch */
+typedef struct CPU_Stage
+{
+    int pc;
+    char opcode_str[128];
+    int opcode;
+    int rs1;
+    int rs2;
+    int rs3; /*source register added for STR instruction */
+    int rd;
+    int imm;
+    int rs1_value;
+    int rs2_value;
+    int rs3_value; /* To store source register value for STR instruction */ 
+    int result_buffer;
+    int memory_address;
+    int has_insn;
+    int stalled; /* Shweta : Indicate if instr is stalled */
+
+} CPU_Stage;
+
+/* Model of APEX CPU */
+typedef struct APEX_CPU
+{
+    int pc;                        /* Current program counter */
+    int clock;                     /* Clock cycles elapsed */
+    int insn_completed;            /* Instructions retired */
+    int regs[REG_FILE_SIZE];       /* Integer register file */
+    int code_memory_size;          /* Number of instruction in the input file */
+    APEX_Instruction *code_memory; /* Code Memory */
+    int data_memory[DATA_MEMORY_SIZE]; /* Data Memory */
+    int single_step;               /* Wait for user input after every cycle */
+    int zero_flag;                 /* {TRUE, FALSE} Used by BZ and BNZ to branch */
+    int fetch_from_next_cycle;
+
+    int simulate;  /*Shweta : To enable simulate function*/
+    int operationCycles; /*Shweta : To run definite cycles*/
+    int registerStatus[REG_FILE_SIZE]; /*Shweta : To store register status*/
+    int forwardRegister[REG_FILE_SIZE]; /*Shweta : To store forwarded register values*/
+    int memLoc; /*Shweta : Show value at particular memory location*/
+    int showMem; /*Shweta : Show value at particular memory location*/
+    /* Pipeline stages */
+    CPU_Stage fetch;
+    CPU_Stage decode;
+    CPU_Stage execute;
+    CPU_Stage memory;
+    CPU_Stage writeback;
+} APEX_CPU;
+
+APEX_Instruction *create_code_memory(const char *filename, int *size);
+APEX_CPU *APEX_cpu_init(const char *filename, const char *operation, const int cycles); /*Shweta : Updated the definition*/
+void APEX_cpu_run(APEX_CPU *cpu);
+void APEX_cpu_stop(APEX_CPU *cpu);
+#endif
